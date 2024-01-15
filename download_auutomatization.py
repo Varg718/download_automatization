17f91d40e0615e6e209ed81a549cc1e0cf962cb7
r.wasilewski86@gmail.com <r.wasilewski86@gmail.com>
2024-01-15 14:18:51 +0100

    Addedd missing ebook directory

--- download_auutomatization.py
+++ download_auutomatization.py
@@ -3,14 +3,15 @@ import shutil
 
 default_download_folder = r'/home/robertw/Downloads/'
 
-rar_files = r"/home/robertw/Videos/"
+videos_files = r"/home/robertw/Videos/"
+music_files = r"/home/robertw/Music/"
 ebook_files = r"/home/robertw/Documents/"
 
 try:
     for plik in os.listdir(default_download_folder):
         if plik.endswith((".rar", '.zip', '.7z')):
             source_path = os.path.join(default_download_folder, plik)
-            destination_path = os.path.join(rar_files, plik)
+            destination_path = os.path.join(videos_files, plik)
             shutil.move(source_path, destination_path)
     
     for ebook in os.listdir(default_download_folder):
@@ -18,6 +19,7 @@ try:
             source_path = os.path.join(default_download_folder, ebook)
             destination_path = os.path.join(ebook_files, ebook)
             shutil.move(source_path, destination_path)
+
     
 except Exception as error:
-    print(f"There is an error: {error}")
+    print(f"The error behind is: {error}")
